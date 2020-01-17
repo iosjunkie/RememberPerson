a9f21d469606881253a3d2265f0522822e167468
Jules Lee <jackconsolidated@gmail.com>
2020-01-18 05:00:52 +0800

    Initial Commit

---
+++ RememberPerson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RememberPerson
+//
+//  Created by Jules Lee on 1/18/20.
+//  Copyright © 2020 Jules Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
